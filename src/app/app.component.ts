c8de45f400816b1f79cc24c41f27358e6e2cdd90
Ramprasath93 <ramprasath_kumar@yahoo.com>
2022-06-01 18:53:23 +0530

    fuctionallity added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Reactive-Form-creation';
+  
+
+  //   Email_regex="[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]"
+
+  //formGroup: FormGroup | any;
+  user: FormGroup | any;
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+
+
+  /*  ngOnInit(): void {
+   /*   this.formGroup=new FormGroup({
+       firstName:new FormControl("reterg"),
+       lastName:new FormControl("rhgrhrt")
+ 
+     })
+  
+     this.user=new FormGroup({
+       name:new FormControl('',Validators.required),
+ 
+ 
+       account:new FormGroup({         // Nested Child
+         email:new FormControl('',Validators.required),
+         confirm:new FormControl('',Validators.required),
+       })
+     })
+ 
+    
+ 
+   }  */
+
+  ngOnInit(): void {
+    this.user = this.fb.group({
+      name: ['', Validators.required,],
+      password: ['', [Validators.required,Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{8,32}$')
+    ]],
+      confirmpassword: ['', [Validators.required]],
+
+
+
+      account: this.fb.group({
+        email: ['', [Validators.required,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],    //Validators.pattern(this.Email_regex)
+        confirm: ['', Validators.required],
+      })
+
+    })
+  }
+  onClick(formdata: any) {
+    console.log(formdata)
+    //console.log(this.user.value)
+  }
+
+
 }
---
+++ src/app/custom-validators.ts
@@ -0,0 +1,13 @@
+// import { AbstractControl } from "@angular/forms";
+
+// //Logical implementation
+// //Existing Logic - Javascript Validation code
+
+// export class CustomValidators {
+//     static passwordStrength(control:AbstractControl){
+//         if (CustomValidators.isEmptyValue(control.value)) {
+//             return null;
+//         }
+//             return.control.value.match()
+//     }
+// }
